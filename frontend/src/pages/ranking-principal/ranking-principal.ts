13c6ea38243961de8eb33ba001e42a155b1247c0
Andre Luis Nunes Muniz <alnmuniz@gmail.com>
2018-01-24 22:06:11 -0300

    Inclusão de filtro por nome no ranking principal.

--- frontend/src/pages/ranking-principal/ranking-principal.ts
+++ frontend/src/pages/ranking-principal/ranking-principal.ts
@@ -8,11 +8,39 @@ import { BackendService } from '../../service/backend-service';
 })
 export class RankingPrincipalPage {
     listaRanking: Array<{posicao: number, nome: string, foto: string, pontuacao: number}>;
+    listaRankingOriginal: any;
 
   constructor(public navCtrl: NavController, backend: BackendService) {
         
-    this.listaRanking = backend.obterRanking();
+    this.listaRankingOriginal = backend.obterRanking();
+    this.listaRanking = this.listaRankingOriginal;
 
+  }
+
+  initializeListaRanking() {
+    if (!this.listaRankingOriginal){
+      this.listaRankingOriginal = [];
+    }
+    if (!this.listaRanking){
+      this.listaRanking = [];
+    }
+  }
+
+  getListaRankingFiltrada(ev: any) {
+    // Reset items back to all of the items
+    this.initializeListaRanking();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.listaRanking = this.listaRankingOriginal.filter((item) => {
+        return (item.nome.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    } else {
+      this.listaRanking = this.listaRankingOriginal;
+    }
   }
 
 }
