6214c7b0edf1d47f7b6ca7b5f293c652dad3761f
Piures <anderson.nunes.silva.ans@gmail.com>
2022-10-18 22:58:16 -0300

    Regras - Pontuação ponderada. Ajustes dos widgets e valor dos custos operacionais.

--- source/bolao_app/lib/routes/regra.dart
+++ source/bolao_app/lib/routes/regra.dart
@@ -98,7 +98,7 @@ class _RegraRouteState extends State<RegraRoute> {
                     ),
                     const SizedBox(
                         width: 400,
-                        height: 200,
+                        height: 215,
                         child: Text('A cada ano, o número de participantes foi subindo até que, em 2018, o Bolão da Copa ganhou sua versão para Smartphones e ganhou o mundo, com aparição na TV e com participantes da Inglaterra, EUA, Austrália, Londres, Portugal, etc. O foco sempre será na diversão de familiares, amigos e amigos dos amigos. Jogos aparentemente menos badalados como Marrocos x Irã (2018) se tornam um clássico na nossa brincadeira. Essa será a sétima edição do Bolão da Copa. Esperamos que você se divirta com a gente!',textAlign: TextAlign.justify, style: TextStyle(fontSize: 15),)
                     ),
                     const SizedBox(
@@ -166,7 +166,7 @@ class _RegraRouteState extends State<RegraRoute> {
                     ),
                     const SizedBox(
                         width: 400,
-                        height: 105,
+                        height: 200,
                         child: Padding(
                           padding: EdgeInsets.only(top: 30.0),
                           child: Text('OBS: Para efeito de pontuação, só será considerado o tempo regulamentar (tempo normal de jogo com os acréscimos), não incluindo prorrogação e disputas de pênaltis.',textAlign: TextAlign.justify, style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
@@ -174,7 +174,7 @@ class _RegraRouteState extends State<RegraRoute> {
                     ),
                     const SizedBox(
                         width: 400,
-                        height: 70,
+                        height: 80,
                         child: Center(child: Text('Premiação',textAlign: TextAlign.center, style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),))
                     ),
                     const SizedBox(
@@ -232,7 +232,7 @@ class _RegraRouteState extends State<RegraRoute> {
                     ),
                     const SizedBox(
                         width: 400,
-                        height: 135,
+                        height: 155,
                         child: Text('Essa regra tem por objetivo dar transparência à nossa brincadeira. Assim, no dia de cada jogo, a inclusão ou a alteração de apostas estarão bloqueadas, e as apostas de todos os participantes estarão disponíveis para consulta. Todo mundo acompanha as apostas de todo mundo, podendo torcer a favor (ou contra) um placar, a depender da situação.',textAlign: TextAlign.justify, style: TextStyle(fontSize: 15),)
                     ),
 
